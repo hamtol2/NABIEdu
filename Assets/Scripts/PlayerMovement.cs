bffb31338f2c0e7d99a20810caeab61dd008dc33
RonnieJ <ronniej@naver.com>
2019-08-08 17:16:01 +0900

    Day2 Complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform myTransform;   // 트랜스폼 컴포넌트.
 
+    public Animator animator;       // Animator 컴포넌트.
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,22 @@ public class PlayerMovement : MonoBehaviour
 
         // 좌우 입력.
         float h = Input.GetAxis("Horizontal");
+
+        // 애니메이션 설정.
+        // || -> 또는.
+        // v가 0이 아니거나 h가 0이 아니면
+        // -> 입력이 있으면.
+        if (v != 0f || h != 0f)
+        {
+            // 달리기 애니메이션 재생.
+            animator.SetInteger("state", 1);
+        }
+        else
+        {
+            // 멈추는 애니메이션 재생.
+            animator.SetInteger("state", 0);
+        }
+
 
         // 앞/뒤로 이동.
         //myTransform.position =
