1dcd5711f231234c227098fcc31be99263ca1ae9
RonnieJ <ronniej@naver.com>
2019-08-08 16:04:44 +0900

    Day2 ing 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 3f;    // 3m/sec.
+    public float moveSpeed = 3f;    // 3 미터/초.
+    public float turnSpeed = 360f;  // 360 도/초.
+
     public Transform myTransform;   // 트랜스폼 컴포넌트.
 
     // Start is called before the first frame update
@@ -21,13 +23,30 @@ public class PlayerMovement : MonoBehaviour
         // W/위:1, S/아래:-1, 없음:0, 
         float v = Input.GetAxis("Vertical");
 
-        // 
+        // 좌우 입력.
+        float h = Input.GetAxis("Horizontal");
 
         // 앞/뒤로 이동.
+        //myTransform.position =
+        //    myTransform.position
+        //    + new Vector3(0f, 0f, v * moveSpeed)
+        //    * Time.deltaTime;
+        //myTransform.position =
+        //    myTransform.position
+        //    //+ new Vector3(0f, 0f, 1f) * v * moveSpeed
+        //    + Vector3.forward * v * moveSpeed
+        //    * Time.deltaTime;
+
         myTransform.position =
             myTransform.position
-            + new Vector3(0f, 0f, v * moveSpeed)
+            + myTransform.forward * v * moveSpeed
             * Time.deltaTime;
+
+        // 좌/우 회전.
+        myTransform.Rotate(
+            Vector3.up,
+            turnSpeed * h * Time.deltaTime
+        );
     }
 }
 
